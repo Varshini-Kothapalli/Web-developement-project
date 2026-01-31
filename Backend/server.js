04d96e1040ce3bbd8f435cd6d14feb3d556e51aa
VoggeSatwika <voggesathwika@gmail.com>
2026-01-31 12:29:33 +0530

    budget route imports added to server

--- Backend/server.js
+++ Backend/server.js
@@ -2,21 +2,23 @@ import exp from 'express'
 import { MongoClient,Db } from 'mongodb';
 import cors from "cors";
 
-
-
 const app=exp()
 const port=8080;
 app.use(exp.json())
 app.use(cors());
  
 import { vendorRoute } from './routes/vendorRoutes.js';
+import { budgetRoute } from './routes/budgetplanner.js';
 app.use('/vendor-api',vendorRoute)
+app.use('/budget-api',budgetRoute)
 const dbclient=new MongoClient("mongodb://localhost:27017")
 dbclient.connect()
 .then(()=>{
     const dbobj=dbclient.db('backenddb')
     const vendorscollection=dbobj.collection('vendors')
     app.set("vendorscollection",vendorscollection)
+    const budgetcollection=dbobj.collection('budgetplanner')  
+    app.set("budgetcollection",budgetcollection)
     console.log("Data base connection success")
         app.listen(port,()=>console.log(`Server is listening ${port}`))
 }).
