8f704d2f2b282268a0feb5e6677543fce821f832
Prashasti M <prashastim0412@gmail.com>
2026-01-31 10:53:51 +0530

    Add TouristRoutes API

---
+++ Backend/routes/TouristRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import TouristPlace from "../../../models/TouristPlaces.js";
+
+const router = express.Router();
+
+// add place
+router.post("/add", async (req, res) => {
+  try {
+    const place = await TouristPlace.create(req.body);
+    res.status(201).json(place);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
+router.get("/", async (req, res) => {
+  const places = await TouristPlace.find();
+  res.json(places);
+});
+
+export default router;
